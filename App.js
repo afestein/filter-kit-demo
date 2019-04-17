fea31dbea0b85d53d631b19671c061865653013e
afestein <afestein@gmail.com>
2019-04-17 11:09:45 +1000

    Add a simple image blend

--- App.js
+++ App.js
@@ -6,44 +6,29 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import React, { Component } from "react";
+import { Image } from "react-native";
+import { OverlayBlend } from "react-native-image-filter-kit";
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <OverlayBlend
+        style={{ height: 100, width: 100 }}
+        dstImage={
+          <Image
+            style={{ height: 200, width: 200 }}
+            source={{ uri: "https://i.imgur.com/iPKTONG.jpg" }}
+          />
+        }
+        srcImage={
+          <Image
+            style={{ height: 200, width: 200 }}
+            source={{ uri: "https://i.imgur.com/p4NM6fY.png" }}
+          />
+        }
+      />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
